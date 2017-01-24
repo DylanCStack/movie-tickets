de789a515bbc221c0e528d2b1ff9b1283af40cf2
Dylan Stackhouse and Jeremiah Freeman <jeremiah@>
2017-01-24 13:39:18 -0800

    add showing obj constructor, take input for ticket creation

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,44 @@
-function ticket(title, release, showtime, ageGroup, discounts, amount) {
-  this.title = title;
+function ticket(titles, release, showtime, ageGroup, discounts, amount) {
+  this.titles = titles;
   this.release = release;
   this.showtime = showtime;
   this.ageGroup = ageGroup;
   this.discounts = discounts;
   this.amount = amount;
 }
-
+function showing(titles, release, showtime) {
+  this.titles = titles;
+  this.release = release;
+  this.showtime = showtime;
+}
 
 
 
 $(document).ready(function() {
+
+  $(".movieTitle").click(function(){
+    $(this).siblings().children("label").children("input").prop('disabled',false);
+    $(this).siblings().show();
+  })
+
+
   $("form#movieTickets").submit(function(event) {
     event.preventDefault();
-    var showtime = $("input:radio[name=showtime]:checked").val();
-    var age = $("#")
+    var movieTitles = [];
+    $("input:checkbox:checked").each(function(){
+
+      movieTitles.push($(this).val());
+    });
+    console.log(movieTitles);
+    var showtime = []
+    $("input:radio[name=showtime]:checked").each(function(){
+
+      showtime.push($(this).val());
+    });
+    console.log(showtime);
+    var ageDiscount = parseInt($("#ageGroup").val());
+    var demographicDiscount = parseInt($("#discount").val());
+
 
   })
 })
